113c549b542ac536f91c1242de4d6eee6e755c5b
Florian Kasper <florian.kasper@corscience.de>
2012-12-23 12:48:49 +0100

    variable procession added

--- libs/Lib/Router/Router.php
+++ libs/Lib/Router/Router.php
@@ -11,17 +11,53 @@
 
 namespace Lib\Router;
 
+use Lib\Request\RequestUtils,
+	Lib\Request\RequestSecurity;
+
 class Router implements RoutingInterface{
 	
+	/**
+	 * Symfony's Request object (already checked)
+	 * @var Request
+	 */
 	private $request;
 	
+	/**
+	 * Symfony's Response object (already checked)
+	 * @var Response
+	 */
 	private $response;
 	
+	/**
+	 * The configuration
+	 * @var StdClass
+	 */
 	private $config;
 	
+	/**
+	 * Constructor.
+	 * 
+	 * @param Request $request
+	 * @param Response $response
+	 * @param StdClass $config
+	 */
 	public function __construct($request,$response,$config){
-		
+		$this->processVariables($request,$response,$config);
 	}
+	
+	/**
+	 * Variable procession
+	 * 
+	 * @param Request $request
+	 * @param Response $response
+	 * @param StdClass $config
+	 */
+	public function processVariables($request,$response,$config){
+		$this->request = $request;
+		$this->response = $response;
+		$this->config = $config;
+	}
+	
 	public function route(){
 		
 	}
